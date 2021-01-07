fc5b5d9e86d7c9f48c9837905788da99887fbec6
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-06 18:56:07 -0600

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+function wakeDog(dogName, dogBreed) {
+    return `Wake ${dogName} the ${dogBreed}`
+}
+function leashDog(dogName, dogBreed){
+    return `Leash ${dogName} the ${dogBreed}`
+}
+function walkToPark(a, b){
+    return `Walk to the park with ${a} the ${b}`
+}
+function throwFrisbee(a,b){
+    return `Throw the frisbee for ${a} the ${b}`
+}
+function walkHome(a,b){
+    return `Walk home with ${a} the ${b}`
+}
+function unleashDog(a,b){
+    return `Unleash ${a} the ${b}`
+}
+
+function exerciseDog(dogName, dogBreed){
+    let array = [];
+    for(let i=0; i< routine.length; i++){
+        array.push(routine[i](dogName, dogBreed));
+    }
+    return array
+}
+
